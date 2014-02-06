4dbc40ab524d29f32a3a67ca3faf66a472cf879b
hardBox <hardBox@users.noreply.github.com>
2014-02-05 20:44:45 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+#
+#
+#################################################
+
+# must check for dictionary file  /usr/share/dict/words
+# or download and install one
+echo "[] Checking for dictionary file"
+
+
+
+
+echo "[] Setting up hostname rewrites"
+
+sudo cp hostname-rewrite-files /etc/init.d/hostname-rewrite-files
+sudo chmod u+x /etc/init.d/hostname-rewrite-files
+sudo update-rc.d hostname-rewrite-files start 36 S .
+
+sudo cp hostname.sh /etc/init.d/hostname.sh
+sudo chmod u+x /etc/init.d/hostname.sh
+sudo update-rc.d hostname.sh start 02 S .
+
+
+echo "[] Setting up randmom MAC addresses at boot"
+cp change-mac.sh /etc/init.d/change-mac.sh
+chmod +x /etc/init.d/change-mac.sh
+update-rc.d change-mac.sh defaults
