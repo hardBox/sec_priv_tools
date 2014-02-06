4d7a985f0f61f99d5cb68a9e790ee7f4ba287e01
hardBox <hardBox@users.noreply.github.com>
2014-02-05 19:31:18 -0800

    Update change-mac.sh

--- change-mac.sh
+++ change-mac.sh
@@ -3,20 +3,20 @@
 # cp change-mac.sh /etc/init.d/change-mac.sh
 # chmod +x /etc/init.d/change-mac.sh
 # update-rc.d change-mac.sh defaults
-
+#
+################################################
 
     # Disable the network devices
-    ifconfig eth0 down
-    ifconfig wlan0 down
-    ifconfig wlan1 down
-    ifconfig wlan2 down
+ifconfig eth0 down
+ifconfig wlan0 down
+ifconfig wlan1 down
 
     # Spoof the mac addresses
-    /usr/local/bin/macchanger -r eth0
-    /usr/local/bin/macchanger -A wlan0
-    /usr/local/bin/macchanger -A wlan1
-    /usr/local/bin/macchanger -A wlan2
+/usr/local/bin/macchanger -r eth0
+/usr/local/bin/macchanger -A wlan0
+/usr/local/bin/macchanger -A wlan1
 
-    # Re-enable the devices
+    # Re-enable the devices if wanted
 #    ifconfig eth0 up
 #    ifconfig wlan0 up
+#    ifconfig wlan1 up
